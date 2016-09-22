af2cfa5358a7f34dbe6b8c1f44b215ce8d97f07a
Unknown <little_miss_kris07@hotmail.com>
2016-09-21 19:24:13 -0600

    Star Catcher Game Prototype 2

---
+++ Star Catcher Prototypes/Assets/MoveCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Vector3 tempPos;
+
+
+    void Update()
+    {
+        tempPos.x = speed * Time.deltaTime;
+        transform.Translate(tempPos);
+    }
+}
---
+++ Star Catcher Prototypes/Assets/StaticVars.cs
@@ -0,0 +1,8 @@
+﻿
+
+public class StaticVars
+{
+    public static float distance = 10;
+    public static float nextSectionPosition = 30;
+
+}
